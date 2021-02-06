6896b8e4865c8b30fcdb78d6e18be1c73bd5c5f3
Evgeny Kazusev <55188582+i0rdan@users.noreply.github.com>
2021-02-06 20:31:29 +0300

    feat: add code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(typeof array != "undefined" && array.length != 0)  {
+    let x = array[0];
+	  for(y = 0; y < array.length; y++) {
+		  if(x > array[y])
+			  x = array[y];
+	  }
+	  return x;
+  }
+	return 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(typeof array != "undefined" && array.length != 0)  {
+    let x = array[0];
+	  for(y = 0; y < array.length; y++) {
+		  if(x < array[y])
+			  x = array[y];
+	  }
+	  return x;
+  }
+	return 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(typeof array != "undefined" && array.length != 0)  {
+    let x = 0;
+	  for(y = 0; y < array.length; y++) {
+	     x += array[y];
+	  }
+	  return x / array.length;
+  }
+	return 0;
 }
